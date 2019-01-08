35dcf7b96886c8e707ebf85870c9fe75064fb238
Douglas Kato <douglaskato@Douglas.local>
2019-01-08 15:51:04 +0300

    chore(upgrade django): upgrade Django from 1.1 to 2.1.5 and setup project
    - upgrade the django framework version to fix the application's security issues
    - have the application have full support of python 3.5 and above
    - add a JWTAuthentication class to fix for the authentication error
    - change the urls in the urls.py file to use path
    
    [Finishes #162725244]

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -2,9 +2,12 @@
 #
 # from django.conf import settings
 #
-# from rest_framework import authentication, exceptions
+from rest_framework import authentication, exceptions
 #
 # from .models import User
 
 """Configure JWT Here"""
-
+class JWTAuthentication(authentication.BaseAuthentication):
+    
+    def authenticate(self, request):
+        pass
