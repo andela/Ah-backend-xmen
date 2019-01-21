c746ea43a6e8b0422fcb0ab173b0b90ea8df2ef0
mariah <mariah.kakande@gmail.com>
2019-01-21 20:15:53 +0300

    feat(articles):add pagination support for articles   - reduce the code length   - change to limitoffsetpagination   - remove articles per page maximum limit   - change offset to display page   - remove unnecessary imports   - set the first display index   - remove unused variables [Finishes #162725268]

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -2,14 +2,16 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Article
 from django.shortcuts import get_object_or_404
-from rest_framework import serializers
 from .serializers import ArticleSerializer, ArticleUpdateSerializer
 from .renderers import ArticleJSONRenderer
 from rest_framework import status
 from rest_framework.response import Response
 from authors.apps.utils.messages import error_messages
 from authors.apps.profiles.models import Profile
-from authors.apps.utils.custom_permissions.permissions import check_if_is_author
+from authors.apps.utils.custom_permissions.permissions import (
+    check_if_is_author,
+)
+from .paginators import ArticleLimitOffSetPagination
 
 
 class ArticleListCreateView(generics.ListCreateAPIView):
@@ -17,6 +19,7 @@ class ArticleListCreateView(generics.ListCreateAPIView):
     serializer_class = ArticleSerializer
     permission_classes = (IsAuthenticated,)
     renderer_classes = (ArticleJSONRenderer,)
+    pagination_class = ArticleLimitOffSetPagination
 
     def perform_create(self, serializer):
         serializer.save(
@@ -44,5 +47,6 @@ class ArticleUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
         instance = self.get_object()
         check_if_is_author(instance, self.request)
         self.perform_destroy(instance)
-        return Response({"message": error_messages['delete_msg'].format('Article')},
-                        status=status.HTTP_200_OK)
+        return Response(
+            {"message": error_messages['delete_msg'].format('Article')},
+            status=status.HTTP_200_OK)
