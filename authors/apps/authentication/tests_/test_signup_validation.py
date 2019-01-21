60a3922b5ffcd9a422e40629531dcd9eba9dd0e5
d-rita <drnanyanzi@gmail.com>
2019-01-21 16:09:50 +0300

    fix(signup): Validate username - Validate length of username - Validate against username starting with a number - Validate against special characters in username - Write descriptive error messages - Write tests to test validation functionality - Resolve conflicts [Fixes #163298000]

--- authors/apps/authentication/tests_/test_signup_validation.py
+++ authors/apps/authentication/tests_/test_signup_validation.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from rest_framework import status
 
 from authors.apps.authentication.models import User
-from .test_data import responses
+from .test_data import responses, invalid_user_data
 from .test_base_class import BaseTestClass
 
 
@@ -15,6 +15,8 @@ class TestSignupValidation(BaseTestClass):
     - Registration with a very short password
     - Registration with non alphanumeric password
     - Registration with an already existing email
+    - Registration with an already used username
+    - Registration with an invalid username
 
     """
 
@@ -26,7 +28,7 @@ class TestSignupValidation(BaseTestClass):
         """
         expected_response = responses['password_is_too_short']
         resp = self.client.post(reverse('authentication:signup'),
-                                content_type='application/json', data=json.dumps(self.user_data_short_password))
+                                content_type='application/json', data=json.dumps(invalid_user_data['short_password']))
         self.assertDictEqual(resp.data, expected_response)
        
     def test_signup_with_invalid_password_fails(self):
@@ -38,7 +40,7 @@ class TestSignupValidation(BaseTestClass):
         """
         expected_response = responses['password_is_weak']
         resp = self.client.post(reverse('authentication:signup'),
-                                content_type='application/json', data=json.dumps(self.user_data_weak_password))
+                                content_type='application/json', data=json.dumps(invalid_user_data['weak_password']))
         self.assertDictEqual(resp.data, expected_response)
       
 
@@ -53,7 +55,7 @@ class TestSignupValidation(BaseTestClass):
         self.client.post(reverse('authentication:signup'),
                          content_type='application/json', data=json.dumps(self.user_data))
         resp = self.client.post(reverse('authentication:signup'),
-                                content_type='application/json', data=json.dumps(self.same_email_user))
+                                content_type='application/json', data=json.dumps(invalid_user_data['same_email_user']))
         self.assertDictEqual(resp.data, expected_response)
        
 
@@ -67,6 +69,40 @@ class TestSignupValidation(BaseTestClass):
         self.client.post(reverse('authentication:signup'),
                          content_type='application/json', data=json.dumps(self.user_data))
         resp = self.client.post(reverse('authentication:signup'),
-                                content_type='application/json', data=json.dumps(self.same_username_user))
+                                content_type='application/json', data=json.dumps(invalid_user_data['same_username_user']))
         self.assertDictEqual(resp.data, expected_response)
        
+    def test_signup_with_invalid_username_fails(self):
+        """ This function tests that a new user cannot sign up with an invalid username
+
+        Asserts:
+            - "Username can only contain letters and numbers."
+        """
+        expected_response = responses['invalid_username']
+        resp = self.client.post(reverse('authentication:signup'),
+                         content_type='application/json', data=json.dumps(invalid_user_data['invalid_username']))
+        self.assertDictEqual(resp.data, expected_response)
+
+    def test_signup_with_short_username_fails(self):
+        """ This function tests that a new user cannot sign up with a short username
+
+        Asserts:
+            - "Username must be longer than 5 characters."
+        """
+        expected_response = responses['short_username']
+        resp = self.client.post(reverse('authentication:signup'),
+                         content_type='application/json', data=json.dumps(invalid_user_data['short_username']))
+        self.assertDictEqual(resp.data, expected_response)
+
+    def test_signup_with_numeric_first_character_username_fails(self):
+        """ 
+        This function tests that a new user cannot sign up with
+        a username starting with a number
+
+        Asserts:
+            - "Username must start with a letter."
+        """
+        expected_response = responses['numeric_first_char_username']
+        resp = self.client.post(reverse('authentication:signup'),
+                         content_type='application/json', data=json.dumps(invalid_user_data['numeric_first_char_username']))
+        self.assertDictEqual(resp.data, expected_response)
