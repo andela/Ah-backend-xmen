9346da34d390616be8c207169ccd2a2d6cd6d518
Armstrong Muhwezi <armstrongsouljah@gmail.com>
2019-01-21 20:10:11 +0300

    feat(authors): List authors profiles (#21)
    - create endpoint to list existing authors profiles
    - restrict access to only logged in users
    - test to ensure access is only granted to authenticated users
    - Document unit tests.
    - render profile data with consistency
    - remove debugging statements
    
    [Finishes #162725266]

--- authors/apps/profiles/serializers.py
+++ authors/apps/profiles/serializers.py
@@ -23,12 +23,58 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 
 class ProfileUpdateSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField(source="User")
     class Meta:
+        extra_kwargs ={
+            'username':{
+                'read_only':True
+            }
+        }
         model = Profile
         fields = [
+            'username',
             'first_name',
             'last_name',
             'bio',
             'image',
             'date_of_birth'
         ]
+   
+    
+    def get_username(self, obj):
+        return f"{obj.user.username}"
+
+    def to_representation(self, instance):
+        """
+         Args: 
+           instance :(Django model instance)
+        
+        Returns: 
+            Dictionary representation of the custom object
+            This is to allow us customize how the serialized data is rendered
+        """
+        return {
+            "profile":{
+                "first_name":instance.first_name, 
+                "last_name": instance.last_name,
+                "bio": instance.bio,
+                "image":instance.image or None, # put null if no image was uploaded
+                "date_of_birth": instance.date_of_birth
+             } 
+        }
+
+
+class ProfileListSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+    class Meta:
+        model = Profile
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'bio',
+            'image'
+        ]
+    
+    def get_username(self, obj):
+        return f"{obj.user.username}"
--- authors/apps/profiles/urls.py
+++ authors/apps/profiles/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import (
     UserProfileView,
     UserProfileUpdateView,
+    UserProfileListView,
 )
 
 
 urlpatterns = [
+    path('', UserProfileListView.as_view(), name='profile-list'),
     path('<username>', UserProfileView.as_view(), name='profile-detail'),
     path('<username>/edit', UserProfileUpdateView.as_view(), name='profile-update')
 ]
