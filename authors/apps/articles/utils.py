eeab936fe539dd65df7e0c974b5b243b58985ae3
calvinpete <36506567+calvinpete@users.noreply.github.com>
2019-01-24 17:03:10 +0300

    feat(articles): Like or dislike articles (#24)
    - Create an ArticleLikes model
    - Create likes_count and dislikes_count methods for the Article model
    - Set up a get_likes_or_dislikes function to count the number of likes/dislikes
    - Set up the ArticleLikeView api endpoint
    - create the path for the endpoint
    - write unit tests for like-dislike functionality
    
    [Finishes #162725262]

--- authors/apps/articles/utils.py
+++ authors/apps/articles/utils.py
@@ -1,6 +1,7 @@
 import random
 import string
 from django.utils.text import slugify
+from django.contrib.auth import get_user_model
 
 
 def unique_code_generator(size=5, chars=string.ascii_lowercase + string.digits):
@@ -9,3 +10,59 @@ def unique_code_generator(size=5, chars=string.ascii_lowercase + string.digits):
 
 def generate_slug(instance):
     return slugify(instance.title)
+
+
+def get_likes_or_dislkes(**kwargs):
+    """
+    Fetches the number of likes and dislikes
+
+    args:
+        Keyword arguments: These are include the model, id of an article and
+                            the like status of the article
+
+    Returns:
+        filtered_likes(int): number of likes or dislikes of an article
+    """
+    likes = kwargs.get('model').objects.all().filter(
+        like_article=kwargs.get('like_article')
+    )
+    filtered_likes = likes.filter(article_id=kwargs.get('article_id'))
+    return filtered_likes.count()
+
+
+def get_like_status(like_status, arg_1, arg_2):
+    """
+    Fetches a string for a particluar like_status
+
+    args:
+        like_status(boolean): It's either a True for a like
+                                or False for a dislike
+        arg_1(str)
+        arg_2(str)
+
+    Returns:
+        arg_1(str): Incase the like_status is True
+        arg_2(str): Incase the like_status is False
+    """
+    return arg_1 if like_status else arg_2
+
+
+def get_usernames(**kwargs):
+    """
+    Fetches a list of usernames
+
+    args:
+        Keyword arguments: These are include the model, id of an article and
+                            the like status of the article
+
+    Returns:
+        usernames(list): usernames of registered users
+    """
+    filtered_list = kwargs.get('model').objects.all().filter(
+        article_id=kwargs.get('article_id')
+    ).filter(like_article=kwargs.get('like_article'))
+    usernames = []
+    for likes in filtered_list:
+        usernames.append(get_user_model().objects.get(
+            pk=likes.user.id).username)
+    return usernames
--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import Article
+from .models import Article, ArticleLikes
 from django.shortcuts import get_object_or_404
 from .serializers import ArticleSerializer, ArticleUpdateSerializer
 from .renderers import ArticleJSONRenderer
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.response import Response
 from authors.apps.utils.messages import error_messages
 from authors.apps.profiles.models import Profile
@@ -12,6 +12,8 @@ from authors.apps.utils.custom_permissions.permissions import (
     check_if_is_author,
 )
 from .paginators import ArticleLimitOffSetPagination
+from .utils import get_like_status, get_usernames
+from rest_framework.exceptions import NotFound
 
 
 class ArticleListCreateView(generics.ListCreateAPIView):
@@ -37,6 +39,7 @@ class ArticleUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
         return {
             'request': self.request
         }
+
     def get_object(self):
         slug = self.kwargs.get('slug')
         return get_object_or_404(Article, slug=slug)
@@ -45,7 +48,7 @@ class ArticleUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
         article = self.get_object()
         check_if_is_author(article, self.request)
         serializer.save(
-         author=Profile.objects.get(user=self.request.user)
+            author=Profile.objects.get(user=self.request.user)
         )
 
     def destroy(self, *args, **kwargs):
@@ -54,4 +57,131 @@ class ArticleUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
         self.perform_destroy(instance)
         return Response(
             {"message": error_messages['delete_msg'].format('Article')},
+            status=status.HTTP_200_OK)
+
+
+class ArticleLikesView(generics.RetrieveUpdateDestroyAPIView):
+    """Updates, retrieves and deletes an articlelikes instance"""
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (ArticleJSONRenderer,)
+
+    def put(self, request, slug):
+        """
+        Updates the article with the reader's feedback
+
+        args:
+            request (Request object): Django Request context
+            slug (Article label): stores and generates a valid URL for the
+                                    article.
+
+        Returns:
+            HTTP Response message: A dictionary
+            HTTP Status code: 201, 200
+        """
+
+        user = request.user
+        article_id = get_object_or_404(Article, slug=slug)
+        like_article = request.data.get('like_article', None)
+
+        if like_article is None:
+            raise serializers.ValidationError(
+                'like_article field is required')
+
+        if type(like_article) != bool:
+            raise serializers.ValidationError(
+                'Value of like_article should be a boolean')
+
+        try:
+            like = ArticleLikes.objects.get(
+                user=user, article=article_id, like_article=like_article)
+
+            verb = get_like_status(like_article, 'liked', 'disliked')
+            return Response(
+                {'message': 'You have already {} the article'.
+                    format(verb)},
+                status=status.HTTP_200_OK)
+        except ArticleLikes.DoesNotExist:
+            like = ArticleLikes.objects.create(
+                user=user, article=article_id, like_article=like_article)
+            like.save()
+
+            if ArticleLikes.objects.filter(
+                user=user,
+                article=article_id
+            ).count() > 1:
+                first_like = ArticleLikes.objects.get(
+                    user=user,
+                    article=article_id,
+                    like_article=not like_article
+                )
+                first_like.delete()
+
+            verb = get_like_status(like_article, 'liked', 'disliked')
+            return Response(
+                {'message': 'You have {} an article'.format(verb)},
+                status=status.HTTP_201_CREATED)
+
+    def delete(self, request, slug):
+        """
+        Removes the reader's feedback on the article
+
+        args:
+            request (Request object): Django Request context
+            slug (Article label): stores and generates a valid URL for the
+                                    article.
+
+        Returns:
+            HTTP Response message
+            HTTP Status code: 200
+        """
+
+        user = request.user
+        article_id = get_object_or_404(Article, slug=slug)
+
+        try:
+            like_ = ArticleLikes.objects.get(
+                user=user, article=article_id)
+            like_article = ArticleLikes.objects.filter(
+                user=user,
+                article=article_id
+            ).values('like_article')[0].get('like_article')
+            like_.delete()
+            verb = get_like_status(like_article, 'unliked', 'un-disliked')
+            return Response(
+                {'message': 'You have {} an article'.
+                    format(verb)},
+                status=status.HTTP_200_OK)
+        except ArticleLikes.DoesNotExist:
+            raise serializers.ValidationError(
+                "There is no like or dislike to remove")
+
+    def get(self, request, slug):
+        """
+        Fetches a list of readers that gave feedback to an article
+
+        args:
+            request (Request object): Django Request context
+            slug (Article label): stores and generates a valid URL for the
+                                    article.
+
+        Returns:
+            HTTP Response message
+            HTTP Status code: 200
+        """
+
+        article_id = get_object_or_404(Article, slug=slug)
+
+        pleasured_users = get_usernames(
+            model=ArticleLikes,
+            article_id=article_id,
+            like_article=True
+        )
+        displeasured_users = get_usernames(
+            model=ArticleLikes,
+            article_id=article_id,
+            like_article=False
+        )
+        return Response(
+            {'likes': pleasured_users,
+                'dislikes': displeasured_users},
             status=status.HTTP_200_OK)
