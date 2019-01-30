90e5b84acd520ff317fa8285b18ac58903f57b3b
mariamiah <mariah.kakande@gmail.com>
2019-01-30 12:35:42 +0300

    feat(articles): make page index start at one (#39)
    [Finishes #163567754]

--- authors/apps/articles/paginators.py
+++ authors/apps/articles/paginators.py
@@ -1,6 +1,7 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 
 
-class ArticleLimitOffSetPagination(LimitOffsetPagination):
-    default_limit = 10
-    offset_query_param = 'page'
+class ArticlePageNumberPagination(PageNumberPagination):
+    page_size = 10
+    page_query_param = 'page'
+    page_size_query_param = 'limit'
